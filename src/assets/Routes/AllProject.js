55e24ac15ae2bceee21011454f5394214d5a87a8
XuyenTN <tranxuyen08@gmail.com>
2023-10-16 17:20:35 +0700

    fix project

--- src/assets/Routes/AllProject.js
+++ src/assets/Routes/AllProject.js
@@ -3,7 +3,7 @@ const allProjects = [
     title: 'Fresh Food',
     img: require('../img/fresh-food-opal.vercel.app_.webp'),
     description:
-      'Meal Map is a website to search food recipes around the world, this website uses the edamam API.',
+      `This website, "Fresh Food," is built using HTML, CSS, and JavaScript, and the database is stored in localStorage. This means that the website's data is stored on the user's computer using the localStorage feature of the web browser. This allows the website to store data locally, such as information about food items and recipes, on the user's computer so they can access it easily without the need for an internet connection.`,
     websiteUrl: 'https://fresh-food-opal.vercel.app/',
     githubUrl: 'https://github.com/tranxuyen08/Projects-Fresh-Food',
     techStack: ['Javascript', 'CSS', 'HTML'],
@@ -12,7 +12,7 @@ const allProjects = [
     title: 'MoonLight NodeJS.',
     img: require('../img/Screen Shot 2023-10-15 at 18.02.17.webp'),
     description:
-      'Meal Map is a website to search food recipes around the world, this website uses the edamam API.',
+      '"MoonLight" is a website built with ReactJs and NodeJS/Express, utilizing MongoDB for data storage. The website integrates Mailer for email sending and provides an intuitive and efficient environment for users.',
     websiteUrl: '',
     githubUrl: 'https://github.com/tranxuyen08/login-with-google-reactts-nodejs',
     techStack: ['Javascript', 'CSS', 'HTML', 'ReactJs', 'NodeJS/Express', 'Sequelize', 'Mailer', 'MongoDB'],
@@ -21,7 +21,7 @@ const allProjects = [
     title: 'MoonLight online movie streaming website (NestJS). ',
     img: require('../img/Screen Shot 2023-10-15 at 18.02.17.webp'),
     description:
-      'Animal Aid is an organization that saves animals from suffering, rehabilitates and releases them back into the wild (Front-End).',
+      '"MoonLight" is a website built with ReactJs and NestJs/TypeORM, utilizing MongoDB for data storage. The website integrates Multer upload image and provides an intuitive and efficient environment for users.',
     websiteUrl: '',
     githubUrl: 'https://github.com/tranxuyen08/web-movie-nestjs',
     techStack: ['Javascript', 'HTML5', 'CSS3', 'ReactJs', 'NestJS', 'MongoDB', 'Multer'],
